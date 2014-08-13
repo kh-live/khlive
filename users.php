141b470490dcef738040cf57f6744e2aa1ebc0d4
lionel voirol <lionel@sinux.ch>
2014-08-13 16:36:56 +0200

    swapped columns in user list

--- users.php
+++ users.php
@@ -23,12 +23,12 @@ $db=file("db/users");
 	}
 		if ($_SESSION['type']!="root" AND $data[4]=="root" AND $_SESSION['cong']==$data[3] ){
 		//we dont let normal admins edit superusers
-		echo '<tr><td>'.$data[0].'</td><td>'.$data[2].'</td><td>'.$data[3].'</td><td>Administrator</td><td>'.$data[6].'</td><td>'.@$data[8].'</td><td>'.$last_login.'</td><td></td></tr>';
+		echo '<tr><td>'.$data[0].'</td><td>'.$data[2].'</td><td>'.@$data[8].'</td><td>'.$last_login.'</td><td>'.$data[3].'</td><td>Administrator</td><td>'.$data[6].'</td><td></td></tr>';
 		}elseif ($_SESSION['type']=="root" OR $_SESSION['cong']==$data[3]){
 		//root sees everything and admins can see their congreg.
 	echo '<tr><td>'.$data[0].'</td>';
 	$tmp="user_".$data[4];
-	echo '<td>'.$data[2].'</td><td>'.$data[3].'</td><td>'.$lng[$tmp].'</td><td>'.$data[6].'</td><td>'.@$data[8].'</td><td>'.$last_login.'</td><td><a href="./user_edit?user='.urlencode($data[0]).'">'.$lng['edit'].'</a> - <a href="./user_delete?user='.urlencode($data[0]).'&pin='.$data[5].'">'.$lng['delete'].'</a></td></tr>
+	echo '<td>'.$data[2].'</td><td>'.@$data[8].'</td><td>'.$last_login.'</td><td>'.$data[3].'</td><td>'.$lng[$tmp].'</td><td>'.$data[6].'</td><td><a href="./user_edit?user='.urlencode($data[0]).'">'.$lng['edit'].'</a> - <a href="./user_delete?user='.urlencode($data[0]).'&pin='.$data[5].'">'.$lng['delete'].'</a></td></tr>
 	';
 	
 		}
