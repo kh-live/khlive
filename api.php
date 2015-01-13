23645bb227c638cab688eeb0179e1282875244b8
lionel voirol <lionel@sinux.ch>
2015-01-13 20:38:57 +0200

    update api

---
+++ api.php
@@ -0,0 +1,12 @@
+<?PHP
+include ("db/config.php");
+if (isset($_GET['q'])){
+	$query=explode("**", urldecode($_GET['q']));
+	if ($query[0]==$api_key){
+		if ($query[1]=="status"){
+		echo "ok";
+		exit;
+		}
+	}
+}
+?>
