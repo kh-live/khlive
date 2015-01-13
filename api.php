55c4ff1a6dac2713838378383b6aebddb7c4a81a
lionel voirol <lionel@sinux.ch>
2015-01-13 21:53:48 +0200

    added encription  for api

--- api.php
+++ api.php
@@ -1,12 +1,21 @@
 <?PHP
+$string="";
 include ("db/config.php");
 if (isset($_GET['q'])){
-	$query=explode("**", urldecode($_GET['q']));
-	if ($query[0]==$api_key){
+$decrypted = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($api_key), base64_decode(urldecode($_GET['q'])), MCRYPT_MODE_CBC, md5(md5($api_key))), "\0");
+	$query=explode("**", $decrypted);
+	if ($query[0]+60>=time()){
 		if ($query[1]=="status"){
-		echo "ok";
+		$string="ok";
+		}
+	
+	if ($string!=""){
+		$encrypted=base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($api_key), $string, MCRYPT_MODE_CBC, md5(md5($api_key))));
+		echo $encrypted;
 		exit;
 		}
 	}
 }
+//$encrypted=base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $string, MCRYPT_MODE_CBC, md5(md5($key))));
+//$decrypted = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($key), base64_decode($encrypted), MCRYPT_MODE_CBC, md5(md5($key))), "\0");
 ?>
