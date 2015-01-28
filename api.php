bc8fb619916e3353a425bef250624fb153e10027
lionel voirol <lionel@sinux.ch>
2015-01-28 22:50:32 +0200

    added fetch user to api

--- api.php
+++ api.php
@@ -7,17 +7,23 @@ $query=explode("**", $decrypted);
 	if ($query[0]+60>=time()){
 		if ($query[1]=="status"){
 		$string="ok";
+		}elseif($query[1]=="fetch_users"){
+		$db=file('db/users');
+			foreach($db as $line){
+			$string.=$line;
+			}
+			$string.="@@@ok";
 		}
 	
 	if ($string!=""){
+	$string=="nothing@@@ko";
+		}
+	}else{
+	$string=="timeout@@@".time();
+	}
 		$encrypted=base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($api_key), $string, MCRYPT_MODE_CBC, md5(md5($api_key))));
 		echo $encrypted;
 		exit;
-		}
-	}else{
-	echo "timeout";
-	echo time();
-	}
 }
 //$encrypted=base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $string, MCRYPT_MODE_CBC, md5(md5($key))));
 //$decrypted = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($key), base64_decode($encrypted), MCRYPT_MODE_CBC, md5(md5($key))), "\0");
