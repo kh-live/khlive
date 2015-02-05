867b167763dd379ae36cb86b74aec16816015221
lionel voirol <lionel@sinux.ch>
2015-02-05 14:50:59 +0200

    update ip script

---
+++ update_ip.php
@@ -0,0 +1,26 @@
+<?PHP
+include ('db/config.php');
+exec ('wget -q -O - http://checkip.dyndns.org', $current_ip);
+$previous_ip=implode("",file($temp_dir.'global_ip'));
+if ($current_ip!=$previous_ip){
+	$key=$master_key;
+	$key2=$api_key;
+	$string=time()."**update**".$server_out;
+	$encrypted=base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $string, MCRYPT_MODE_CBC, md5(md5($key))));
+	$response=file_get_contents('http://kh-live.co.za/api.php?q='.urlencode($encrypted));
+	$decrypted = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($key2), base64_decode($response), MCRYPT_MODE_CBC, md5(md5($key2))), "\0");
+	$dec=explode("@@@", $decrypted);
+	if (@$dec[1]=="ok"){
+	//update succesful
+		if ($dec[0]==$current_ip){
+	$file=fopen($temp_dir.'global_ip','w');
+	fputs($file,$current_ip);
+	fclose($file);
+	}else{
+	echo "error wrong ip";
+	}
+	}else{
+	echo "error synchronising ip";
+	}
+}
+?>
