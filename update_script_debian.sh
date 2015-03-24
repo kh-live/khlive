0764cda3d3717e0c49b7ba0183966da6b63a1328
lionel voirol <lionel@sinux.ch>
2015-03-24 18:23:58 +0200

    test modif update script

--- update_script_debian.sh
+++ update_script_debian.sh
@@ -7,7 +7,6 @@ cp /root/update_dir/khlive/.htaccess /var/www/kh-live/
 dos2unix /var/www/kh-live/* > /dev/null 2>&1
 chmod -R 640 /var/www/kh-live/*
 chown -R asterisk:asterisk /var/www/kh-live/*
-
 cp update_script_debian.sh update.sh
 chmod 700 update.sh
 echo "done"
