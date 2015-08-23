ed37c883bfd02c7fa25f48c91c409cc5a7db59a5
lionel voirol <lionel@sinux.ch>
2015-08-23 22:53:39 +0200

    update script
    make it launchable from any working directory

--- update_script_debian.sh
+++ update_script_debian.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #Git update and apply script
-git pull
+(cd /root/update_dir/khlive && git pull)
 cp /root/update_dir/khlive/*.php /var/www/kh-live/
 cp /root/update_dir/khlive/*.css /var/www/kh-live/
 cp /root/update_dir/khlive/.htaccess /var/www/kh-live/
 dos2unix /var/www/kh-live/* > /dev/null 2>&1
 find /var/www/kh-live/ -type f -print | xargs chmod 640 
 chown -R asterisk:asterisk /var/www/kh-live/*
-cp update_script_debian.sh update.sh
-chmod 700 update.sh
+(cd /root/update_dir/khlive && cp update_script_debian.sh update.sh)
+(cd /root/update_dir/khlive && chmod 700 update.sh)
 chmod +x /var/www/kh-live/config/update.sh
 chmod +x /var/www/kh-live/config/downloader.sh
 echo "done"
