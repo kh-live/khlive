faed7f6859ade72d44256d0f82f27edf94b48b4a
lionel voirol <lionel@sinux.ch>
2015-09-11 17:13:53 +0200

    fix recording while mp3

--- meeting-ajax.php
+++ meeting-ajax.php
@@ -368,7 +368,8 @@ $file=fopen('/tmp/meeting_'.$cong_name.'_admin.call','w');
 	//we must start a script then log
 	if ($stream_type=='mp3'){
 	if ($record=='yes'){
-	exec('arecord -f S16_LE -r 8000 | '.$lame_bin." --preset cbr ".$bitrate."  -b 16 -m m -S - /var/www/kh-live/records/".$_SESSION['cong'].'-'.date('Ymd',time()).'_'.date('His',time()).'.mp3'." | ".$ezstream_bin." -c ".$web_server_root."/kh-live/config/asterisk-ices-".$_SESSION['cong'].".xml > /dev/null &");
+	exec("arecord -f S16_LE -r 8000 | ".$lame_bin." --preset cbr ".$bitrate." -b 16 -m m -S - - | ".$ezstream_bin." -c ".$web_server_root."/kh-live/config/asterisk-ices-".$_SESSION['cong'].".xml > /dev/null &");
+	exec('arecord -f S16_LE -r 8000 | '.$lame_bin." -f -b 16 -m m -S - /var/www/kh-live/records/".$_SESSION['cong'].'-'.date('Ymd',time()).'_'.date('His',time()).'.mp3'." > /dev/null &");
 	}else{
 	exec("arecord -f S16_LE -r 8000 | ".$lame_bin." --preset cbr ".$bitrate." -b 16 -m m -S - - | ".$ezstream_bin." -c ".$web_server_root."/kh-live/config/asterisk-ices-".$_SESSION['cong'].".xml > /dev/null &");
 	}
