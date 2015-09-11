2aefeaf2d786b28ec9b3ee14f4f3020034f9a96d
lionel voirol <lionel@sinux.ch>
2015-09-11 14:39:18 +0200

    direct stream record
    fixed syntax to keep consistency

--- meeting-ajax.php
+++ meeting-ajax.php
@@ -360,7 +360,7 @@ $file=fopen('/tmp/meeting_'.$cong_name.'_admin.call','w');
 	//this is direct-stream
 	//we must start a script then log
 	if ($record=='yes'){
-	exec('arecord -f S16_LE -r 8000 | '.$lame_bin.' -f -b 16 -m m -S - /var/www/kh-live/records/'.$_SESSION['cong'].'-'.date('Ymd',time()).'-'.date('His',time()).'.mp3'." > /dev/null &");
+	exec('arecord -f S16_LE -r 8000 | '.$lame_bin.' -f -b 16 -m m -S - /var/www/kh-live/records/'.$_SESSION['cong'].'-'.date('Ymd',time()).'_'.date('His',time()).'.mp3'." > /dev/null &");
 	}
 	if ($stream_type=='mp3'){
 	exec("arecord -f S16_LE -r 8000 | ".$lame_bin." --preset cbr ".$bitrate." -r -m m -s 8.0 --bitwidth 16 - - | ".$ezstream_bin." -c ".$web_server_root."/kh-live/config/asterisk-ices-".$_SESSION['cong'].".xml"." > /dev/null &");
