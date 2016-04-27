4e8a67b32017c9c2860219e36e8a2f40ae81f652
lionel voirol <lionel@sinux.ch>
2016-04-27 10:22:07 +0200

    make path from config songs

--- song.php
+++ song.php
@@ -1,6 +1,7 @@
 <?PHP
 $max_song_no=150;
 if(session_id()==""){session_start();}
+include "db/config.php"
 if (isset($_GET['song_1'])){
 	if ($_GET['song_1'] >=1 AND $_GET['song_1'] <=$max_song_no) $_SESSION['song_1']=$_GET['song_1'];
 }
@@ -15,7 +16,7 @@ if (isset($_GET['play'])){
 	if ($_GET['play'] >=1 AND $_GET['play'] <=$max_song_no) {
 	exec("/usr/bin/mocp -x");
 	exec("/usr/bin/mocp -S");
-	exec("/usr/bin/mocp -l /var/www/kh-live/kh-songs/iasn_E_".$_GET['play'].".m4a");
+	exec("/usr/bin/mocp -l ".$web_server_root."kh-live/kh-songs/iasn_E_".$_GET['play'].".m4a");
 	if(strstr($_SESSION['meeting_status'],"live")){
 	echo "Playing...";
 	}else{
@@ -27,7 +28,7 @@ if (isset($_GET['play'])){
 	echo "Playing Random...";
 	exec("/usr/bin/mocp -S");
 	exec("/usr/bin/mocp -c");
-	exec("/usr/bin/mocp -a /var/www/kh-live/kh-songs/");
+	exec("/usr/bin/mocp -a ".$web_server_root."kh-live/kh-songs/");
 	exec("/usr/bin/mocp -t shuffle");
 	exec("/usr/bin/mocp -p");
 	}
