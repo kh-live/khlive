d18df5bdef29f5584f37ee29b41e05da2ea6191b
lionel voirol <lionel@sinux.ch>
2016-07-26 21:43:46 +0200

    added quick cong change

--- home.php
+++ home.php
@@ -5,12 +5,39 @@ header("HTTP/1.1 404 Not Found");
 include "404.php";
 exit(); 
 }
+if (($_SESSION['type']=="root") {
+if (isset($_GET['tempcong'])){
+$_SESSION['cong']=urldecode($_GET['tempcong']);
+}
+}
 ?>
 <div id="page">
 <h2><?PHP echo $lng['home'];?></h2>
 <?PHP
 echo $lng['welcome'].' <b>'.$_SESSION['full_name'].'</b> !<br />';
 echo $lng['cong_part'].' : <b>'.$_SESSION['cong'].'</b>.<br />';
+if (($_SESSION['type']=="root") {
+echo 'You can change your cong for this session using the drop down menu : <br />';
+?>
+<script type="text/javascript">
+function changeCong(){
+var a = document.getElementById("tempcong").value;
+if (a!=0)
+window.location="./?tempcong=" + a ;
+}
+</script>
+<?PHP
+echo '<select id="tempcong" name="congregation" onchange="javascript:changeCong(this)" >
+<option value="0">'.$lng['select'].'...</option>';
+
+$db=file("db/cong");
+    foreach($db as $line){
+        $data=explode ("**",$line);
+	echo '<option value="'.$data[0].'">'.$data[0].'</option>';
+	}
+echo '</select><br /><br />';
+
+}
 if ($_SESSION['type']!="manager") echo "Your quick login PIN is : <b>".$_SESSION['pin']."#</b><br /><br />";
 
 if ($_SESSION['type']!="user"){
