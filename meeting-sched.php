98544b347a65b5f86890e477c33f3baeacdcc9a0
lionel voirol <lionel@sinux.ch>
2016-09-27 21:50:28 +0200

    update meeting sched

--- meeting-sched.php
+++ meeting-sched.php
@@ -1,8 +1,26 @@
 <?PHP
 include 'db/config.php';
 if (isset($scheduler)){
-	if ($$scheduler=='yes'){
-	echo 'scheduler worked at '.date('H-i-s',time());
+	if ($scheduler=='yes'){
+	$db=file('db/sched');
+		if ($db!=''){
+			foreach($db as $line){
+				$data=explode('**', $line);
+				$cong=$data[0];
+				$day=$data[1];
+				$start_time=explode(':',$data[2]);
+				$stop_time=explode(':',$data[3]);
+				$enabled=$data[4];
+				if (($enabled=='yes') AND (date('D',time())==$day) AND (date('G',time())==$start_time[0]) AND ( (1*date('i',time())==$start_time[1]) OR ((1*date('i',time())) +1 == $start_time[1]) OR ((1*date('i',time())) -1 == $start_time[1]) ) ){
+				$info=time().'**info**schedule meeting launched successful**'.$cong."**\n";
+	$file=fopen('./db/logs-'.date("Y",time()).'-'.date("m",time()),'a');
+			if(fputs($file,$info)){
+			fclose($file);
+	}
+				}
+			}
+		}
+	
 	}
 }
 ?>
