6ea0b59dbde66631095dd587117e5933dd47fb0c
lionel voirol <lionel@sinux.ch>
2016-09-29 16:47:55 +0200

    faster time

--- user_add.php
+++ user_add.php
@@ -95,7 +95,8 @@ At least 8 characters. Tip : use a sentence!<br />
 <input class="field_login" type="text" name="pin" value="<?PHP echo rand(10000,99999) ;?>">#<br /><br />
 <?PHP
 if ($auto_khlive=='yes' OR $server_beta=='master'){
-$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
+//$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
+$ch = curl_init();
 	if ($server_beta=='master'){
 		$url="";
 		$db=file("db/servers");
@@ -106,14 +107,19 @@ $context = stream_context_create(array('http' => array('header'=>'Connection: cl
 			}
 		}
 		if ($url==""){
-		$test_time=@file_get_contents('http://impossible.kh-live.co.za/time.php',false,$context);
+		curl_setopt($ch, CURLOPT_URL, 'http://impossible.kh-live.co.za/time.php');
+		//$test_time=@file_get_contents('http://impossible.kh-live.co.za/time.php',false,$context);
 		echo 'Could not find your congregations server...';
 		}else{
-		$test_time=@file_get_contents('http://'.$url.'/kh-live/time.php',false,$context);
+		curl_setopt($ch, CURLOPT_URL, 'http://'.$url.'/kh-live/time.php');
+		//$test_time=@file_get_contents('http://'.$url.'/kh-live/time.php',false,$context);
 		}
 	}else{
-		$test_time=@file_get_contents('http://kh-live.co.za/time.php',false,$context);
+		curl_setopt($ch, CURLOPT_URL, 'http://kh-live.co.za/time.php');
+		//$test_time=@file_get_contents('http://kh-live.co.za/time.php',false,$context);
 	}
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$test_time = curl_exec($ch);
 if ($test_time!==FALSE){
 	if (is_numeric($test_time)){
 		$now=time();
