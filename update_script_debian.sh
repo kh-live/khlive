e64900115d96131e979a876e368c36337734b1f8
lionel voirol <lionel@sinux.ch>
2016-10-24 16:54:13 +0200

    fix update script for sing apostroph in name

--- update_script_debian.sh
+++ update_script_debian.sh
@@ -5,7 +5,7 @@ cp /root/update_dir/khlive/*.php /var/www/kh-live/
 cp /root/update_dir/khlive/*.css /var/www/kh-live/
 cp /root/update_dir/khlive/.htaccess /var/www/kh-live/
 dos2unix /var/www/kh-live/* > /dev/null 2>&1
-find /var/www/kh-live/ -type f -print | xargs chmod 640 
+find /var/www/kh-live/ -type f -printf '"%p"\n' | xargs chmod 640 
 chown -R asterisk:asterisk /var/www/kh-live/*
 (cd /root/update_dir/khlive && cp update_script_debian.sh update.sh)
 (cd /root/update_dir/khlive && chmod 700 update.sh)
