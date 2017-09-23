92ed709a944a61111f1876458901ad7b9778e495
lionel voirol <lionel@sinux.ch>
2017-09-23 12:18:12 +0200

    bug fix

--- meeting-listeners.php
+++ meeting-listeners.php
@@ -5,11 +5,11 @@
 	$db=array(
 	'1**Listener 1 (over internet stream - 10 people listening together)**start_cong**stream-start_cong.ogg**'.time().'**normal**10**',
 	'2**Listener 2 (over internet stream - wanting to answer to paragraph 1)**start_cong**stream-start_cong.ogg**'.time().'**request--paragraph 1--This is a test answer by a test listener**1**',
-	'3**Listener 3 (over internet stream - answering to paragraph 2)**start_cong**stream-start_cong.ogg**'.time().'**answering--paragraph 2--This is a test answer by a test listener**1**'
+	'3**Listener 3 (over internet stream - answering to paragraph 2)**start_cong**stream-start_cong.ogg**'.time().'**answering--paragraph 2--This is a test answer by a test listener**1**',
 	'4**Listener 4 (over VOIP - click on the cross to disconnect user)**start_cong**phone_live**'.time().'**normal****',
 	'5**Listener 5 (over VOIP - listening to a recorded meeting)**start_cong**phone_record**'.time().'**normal****',
 	'6**Listener 6 (over VOIP - wanting to answer with voice answer)**start_cong**phone_live**'.time().'**request****',
-	'7**Listener 7 (over VOIP - answering)**start_cong**phone_live**'.time().'**answering****',
+	'7**Listener 7 (over VOIP - answering)**start_cong**phone_live**'.time().'**answering****'
 	);
 	}else{
 	$db=file("db/live_users");
